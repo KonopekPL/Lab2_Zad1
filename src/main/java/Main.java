ddc3acd52e3306949e88913d31ab539ab5afc130
KonopekPL <162459532+KonopekPL@users.noreply.github.com>
2024-03-20 13:23:27 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    String hello = ("Hello world!");
+    for (int i = 0; i < hello.length(); i++) {
+      System.out.println(hello.charAt(i));
+    }
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
